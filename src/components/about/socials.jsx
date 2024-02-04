a69e1ddfe6a15927b3efde9f3be5ddee0b8f1eec
Lucas Parada <eng.lucasparada@gmail.com>
2024-02-04 16:43:14 -0300

    feat: Traduzindo

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -35,7 +35,7 @@ const Socials = () => {
 							className="social-icon"
 						/>
 					</div>
-					<div className="social-text">Follow on GitHub</div>
+					<div className="social-text">Siga-me no GitHub</div>
 				</a>
 			</div>
 
@@ -51,7 +51,7 @@ const Socials = () => {
 							className="social-icon"
 						/>
 					</div>
-					<div className="social-text">Follow on LinkedIn</div>
+					<div className="social-text">Siga-me no LinkedIn</div>
 				</a>
 			</div>
 

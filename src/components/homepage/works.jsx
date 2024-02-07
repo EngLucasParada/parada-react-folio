7e05940ff95055c611bd98ac684ca71bcf534f23
EngLucasParada <eng.lucasparada@gmail.com>
2024-02-07 15:45:16 -0300

    feat: inclusão das empresas

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,8 +15,8 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src="./fitoformula.png"
+								alt="fitoformula"
 								className="work-image"
 							/>
 							<div className="work-title">Grupo FitoFórmula</div>
@@ -28,8 +28,8 @@ const Works = () => {
 
 						<div className="work">
 							<img
-								src="./twitter.png"
-								alt="twitter"
+								src="./contrail.png"
+								alt="contrail"
 								className="work-image"
 							/>
 							<div className="work-title">Contrail Logística</div>
@@ -59,15 +59,39 @@ const Works = () => {
 						</div>
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src="./spa.png"
+								alt="spa"
 								className="work-image"
 							/>
-							<div className="work-title">Bradesco</div>
+							<div className="work-title">Santos Port Authority</div>
 							<div className="work-subtitle">
-								Estagiário de BI e Inovação
+								Estagiário de Helpdesk
 							</div>
-							<div className="work-duration">Mar de 2022 - Mai de 2022</div>
+							<div className="work-duration">Set de 2021 - Mar de 2022</div>
+						</div>
+						<div className="work">
+							<img
+								src="./esamc.png"
+								alt="esamc"
+								className="work-image"
+							/>
+							<div className="work-title">ESAMC</div>
+							<div className="work-subtitle">
+								Assistente Administrativo
+							</div>
+							<div className="work-duration">Jan de 2020 - Set de 2022</div>
+						</div>
+						<div className="work">
+							<img
+								src="./CEA.png"
+								alt="CEA"
+								className="work-image"
+							/>
+							<div className="work-title">C&A</div>
+							<div className="work-subtitle">
+								Consultor de produtos financeiros
+							</div>
+							<div className="work-duration">Jul de 2019 - Jan de 2020</div>
 						</div>
 					</div>
 				}

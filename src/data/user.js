bb736d625257acd673c34311538bd9ccb22f4b74
EngLucasParada <eng.lucasparada@gmail.com>
2024-06-03 10:41:15 -0300

    feat

--- src/data/user.js
+++ src/data/user.js
@@ -18,13 +18,13 @@ const INFO = {
 	homepage: {
 		title: "Lucas Parada",
 		description:
-			"Sou Engenheiro de Software com experiência em Ruby on Rails. Tenho experiência na construção de aplicações web escaláveis, seguras e confiáveis usando vários frameworks e tecnologias. Gosto de resolver problemas complexos e aprender novas habilidades. Sou apaixonado por criar código de alta qualidade que siga as melhores práticas e padrões do setor. Estou sempre em busca de novos desafios e oportunidades para crescer!",
+			"Sou Engenheiro de Dados com experiência em Python e SQL. Tenho experiência na modelagem e otimização de bancos de dados, garantindo segurança e confiabilidade. Trabalhei em projetos que envolveram a normalização de grandes conjuntos de dados e a criação de consultas eficientes para melhorar a performance do sistema. Gosto de resolver problemas complexos e aprender novas habilidades. Sou apaixonado por desenvolver soluções de alta qualidade que sigam as melhores práticas e padrões do setor. Estou sempre em busca de novos desafios e oportunidades para crescer, especialmente em ambientes dinâmicos e inovadores.",
 	},
 
 	about: {
 		title: "Sou o Lucas Parada, onde desafios são oportunidades para se destacar.",
 		description:
-			"Atualmente Junior Software Engineer com mais de 3 anos de experiência em desenvolvimento de softwares em Ruby on Rails e APIs RESTful, e ampla familiaridade com a infraestrutura AWS e outras Clouds. Habilidades avançadas em observabilidade, incluindo ferramentas como NewRelic, Grafana e Zabbix, fortalecendo minhas habilidades analíticas e de resolução de problemas. Reconhecido por fornecer suporte eficaz a parceiros externos e compartilhar conhecimento internamente, facilitando a integração e utilização de nossos serviços. Proficiente em bancos de dados como MySQL, PostgreSQL e Oracle, com experiência em otimização e gerenciamento de dados em ambientes de produção. Sou um engenheiro de software comprometido com desempenho, escalabilidade e usabilidade, capaz de trabalhar efetivamente em equipes colaborativas.",
+			"Atualmente, sou Engenheiro de Dados com mais de 3 anos de experiência em desenvolvimento de softwares e análise de dados. Tenho habilidades sólidas em Python, SQL e Spark, e experiência em criação de processos de ETL utilizando Databricks. Recentemente, trabalhei em um projeto onde desenvolvi dashboards de análise financeira utilizando Power BI, o que ajudou a aumentar a visibilidade e a precisão das informações para a equipe de liderança. Além disso, tenho familiaridade com infraestrutura em AWS e Azure, utilizando Docker e Kubernetes para DevOps e CI/CD. Sou comprometido com a qualidade e eficiência dos dados, buscando sempre otimizar processos e contribuir para a tomada de decisão estratégica da empresa. Estou em constante aprendizado e desenvolvimento, buscando crescer na área de engenharia de dados e análise de dados.",
 	},
 
 	articles: {
@@ -37,7 +37,7 @@ const INFO = {
 		{
 			title: "Projeto SIM (TMS) ",
 			description:
-				"Na Contrail, minha atuação abrangeu a sustentação e desenvolvimento de novas funcionalidades, tecnologias usadas Ruby on Rails, jQuery, Bootstrap, Sidekiq, Docker, MySQL, etc…",
+				"Na Contrail, minha atuação abrangeu a sustentação e desenvolvimento de novas funcionalidades, tecnologias usadas Ruby on Rails, jQuery, Bootstrap, Python, Docker, MySQL, etc…",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/ruby/ruby.png",
 			// linkText: "View Project",
 			// link: "https://github.com",
@@ -55,7 +55,7 @@ const INFO = {
 		{
 			title: "Projeto E-commerce",
 			description:
-				"Desenvolvimento do primeiro software do Grupo FitoFórmula, utilizando Ruby on Rails, jQuery, RESTful API, Bootstrap, Docker, MySQL, etc…",
+				"Desenvolvimento do primeiro software do Grupo FitoFórmula e dashboard, utilizando Ruby on Rails, Python, MySQL, PowerBI, etc..",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/ruby/ruby.png",
 			// linkText: "View Project",
 			// link: "https://github.com",
